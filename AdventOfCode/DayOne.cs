fd87c9a96b7bc2058529443593e29022e8b4a050
Lauren J Spring <ljspring@gmail.com>
2020-12-24 08:25:45 +0000

    Add project files.

---
+++ AdventOfCode/DayOne.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace AdventOfCode
+{
+    public class DayOne
+    {
+        public (int, int) GetEntries(int[] expenseReport)
+        {
+            foreach (int entry in expenseReport)
+            {
+                var matchingEntry = expenseReport.Where(x => (x + entry) == 2020).SingleOrDefault();
+                
+                if(matchingEntry != 0)
+                {
+                    return (entry, matchingEntry);
+                }
+            }
+            return (0,0);
+        }
+    }
+}
